c597931460dd6f5b4206ed6190a1648c3fa6e539
Laura Berge <lauraeberge@gmail.com>
2019-08-28 18:32:56 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -129,6 +129,7 @@ def adding_to_dennis
         }
      }
      
-  programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+  programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+  p programmer_hash
 
 end
