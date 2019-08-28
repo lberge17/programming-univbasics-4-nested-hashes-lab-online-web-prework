cde98f757d97bc33d38eb1b09eae78edb95acfea
Laura Berge <lauraeberge@gmail.com>
2019-08-28 18:25:50 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -79,12 +79,12 @@ def adding_matz
           :languages => ["C"]
         }
      }
- programmer_hash.reverse
+     
  programmer_hash[:yukihiro_matsumoto] = {
   :known_for => "Ruby",
   :languages => ["LISP", "C"]
  }
-programmer_hash.reverse
+ 
 p programmer_hash
     
 end
